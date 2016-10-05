4cf7d96d7f85fabe9c6bff3dcdc4e54f78f71904
Petr Štechmüller <stechy12@gmail.com>
2016-10-05 19:35:48 +0200

    Odstranění informace o počtu aktualizačních cyklů na jeden herní cyklus

--- src/cz/zcu/fav/ups/snake/model/World.java
+++ src/cz/zcu/fav/ups/snake/model/World.java
@@ -129,7 +129,7 @@ public final class World {
 
         @Override
         public void handle(long now) {
-            int inLoopTime = 0;
+//            int inLoopTime = 0;
             final long elapsed = now - lastTime;
             lastTime = now;
             lag += elapsed;
@@ -138,10 +138,10 @@ public final class World {
             while (lag >= MS_PER_SECOND) {
                 objects.forEach(object -> object.physicsComponent.handlePhysics(object, lag));
                 lag -= MS_PER_SECOND;
-                inLoopTime++;
+//                inLoopTime++;
             }
 
-            System.out.printf("In loop time: %d%n", inLoopTime);
+//            System.out.printf("In loop time: %d%n", inLoopTime);
 
             GraphicsContext graphic = canvas.getGraphicsContext2D();
             graphic.save();
